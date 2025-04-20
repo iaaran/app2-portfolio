dafa42d68ebe583d1d36ef4855ba40a6d43b3917
iaaran <149783791+iaaran@users.noreply.github.com>
2025-04-20 19:27:39 +0530

    Added description as an exercise

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images\photo.png")
+    st.image("images\\photo.png")
 
 with col2:
     st.title("Aaran")
@@ -13,3 +13,8 @@ with col2:
     Hi, I am Aaran! I am a Python Programmer. My projects developed using python shall be available in this github repository
     """
     st.info(content)
+
+content2 = """
+    Below you can find some of the apps I have built in Python. Feel free to contact me!
+"""
+st.write(content2)
